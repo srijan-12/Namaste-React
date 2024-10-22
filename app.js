d11a2461589d5596b66cb7dca814023b36300bf4
Srijan Sinha <srijansinha@Srijans-MacBook-Air.local>
2024-10-22 05:53:17 +0530

    JSX, react element, react functional components, components compostion

--- app.js
+++ app.js
@@ -2,14 +2,33 @@
 // const ReactDOM = require("react-dom")
 import React from "react"
 import ReactDOM from "react-dom"
-// const heading = React.createElement("h1", {id:"heading", xyz: "abc"}, "Hello from React");
-const parent = React.createElement("div",{id:"parent"},
-                React.createElement("div", {id:"childDiv"},
-                    [React.createElement("h1",{id:"heading1"},"I am h1"),
-                        React.createElement("h2",{id:"heading2"},"I am h2")
-                    ]
-                )
+
+
+//react element
+const elementHeading = (
+    <h1>This is an React element rendered on page</h1>
 )
+// console.log(elementHeading);
+
+
+
+//react element to react functional component
+const Heading = () =>(
+    <h1>This is an React element converted to functional component and rendered on page</h1>
+)
+
+
+//component comosition
+const Title=() =>(
+    <div>
+        <Heading/>
+        <h2>This is title component for component compositioning</h2>
+    </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(heading);
-root.render(parent);
+
+// root.render(elementHeading);
+// root.render(<Heading/>);
+root.render(<Title/>);
